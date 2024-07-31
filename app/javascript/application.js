e3e1ab65e1b312aa756350dcc4999503002dfb24
Rakido <35547354+Rakido@users.noreply.github.com>
2024-08-01 00:49:29 +0200

    yarn add lenis but can't configure

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,17 @@
 import "@hotwired/turbo-rails"
 import "controllers"
+// import Lenis from "lenis"
+
+// const lenis = new Lenis()
+
+// lenis.on('scroll', (e) => {
+//   console.log(e)
+// })
+
+// function raf(time) {
+//   lenis.raf(time)
+//   requestAnimationFrame(raf)
+// }
+
+// requestAnimationFrame(raf)
+// console.log("jkl")
