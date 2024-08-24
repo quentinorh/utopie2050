5468aeed2edcba862a7abc335a806e8ab60e6a2b
Rakido <35547354+Rakido@users.noreply.github.com>
2024-08-24 17:27:40 +0200

    stuck on load scrollTrigger library on show

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -1,12 +1,25 @@
 // app/javascript/controllers/scroll_controller.js
 import { Controller } from "@hotwired/stimulus";
+import Splitting from 'splitting';
+//import ScrollTrigger from "gsap/ScrollTrigger";
 
 export default class extends Controller {
   static targets = ["content", "scrollPercentage"];
 
   connect() {
     this.updateScrollPercentage();
+    Splitting({ target: this.contentTarget, by: 'lines' });
     window.addEventListener("scroll", this.updateScrollPercentage.bind(this));
+
+    // ScrollTrigger.create({
+    //   trigger: element,
+    //   start: "top bottom",
+    //   end: "bottom top",
+    //   scrub:true,
+    //   //markers:true,
+    //   animation: textrev,
+    //   toggleActions: "play none none none",
+    // });
   }
 
   disconnect() {
