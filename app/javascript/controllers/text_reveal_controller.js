c54baa3ca20659c3b4d120fffdecb18901a1f9de
Rakido <35547354+Rakido@users.noreply.github.com>
2024-08-29 01:06:07 +0200

    enhance manifesto on home

---
+++ app/javascript/controllers/text_reveal_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from "@hotwired/stimulus"
+import { gsap } from "gsap"
+import Splitting from "splitting"
+import scrollTrigger from "gsap/scrollTrigger";;
+
+gsap.registerPlugin(scrollTrigger);
+
+export default class extends Controller {
+
+  connect() {
+    this.animateWords()
+  }
+
+  animateWords() {
+    Splitting({ target: this.element, by: 'words' })
+
+    const words = this.element.querySelectorAll('.word')
+    console.log(words)
+    gsap.fromTo(words, 
+      { opacity: .2 },
+      { 
+        opacity: 1, 
+        duration: 1.5, 
+        ease: "power4.out",
+        stagger: 0.1,
+        scrollTrigger: {
+          trigger: this.element, 
+          start: 'top 80%', 
+          end: 'bottom 20%', 
+          toggleActions: 'play none none none', 
+        }
+      })
+  }
+}
