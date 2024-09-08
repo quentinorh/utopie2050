e9c4f9d0c850daa99501b91b93bf2bb7d166cfff
Quentin Orhant <quentin.orhant@gmail.com>
2024-09-08 08:23:55 +0200

    add mail verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Devise modules...
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   # Validations
   validates :username, presence: true, uniqueness: true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,23 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+
+  config.action_mailer.default_url_options = { host: 'https://sp2050.org', protocol: 'https' }
+  Rails.application.routes.default_url_options[:host] = 'https://sp2050.org'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default charset: 'utf-8'
+  config.action_mailer.smtp_settings = {
+    address: 'mail.gandi.net',
+    port: 587,
+    domain: 'sp2050.org',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['EMAIL_USERNAME'],
+    password: ENV['EMAIL_PASSWORD']
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
