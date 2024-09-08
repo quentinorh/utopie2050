3e57dbab6a448773c7acce96f85ad8f0ca74090f
Quentin Orhant <quentin.orhant@gmail.com>
2024-09-08 16:33:36 +0200

    try to fix stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,6 @@
 // Import and register all your controllers from the importmap under controllers/*
 
-import { application } from "controllers/application"
+import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
