262e24f19096151631a11c67acce685c0d2243b9
Quentin Orhant <quentin.orhant@gmail.com>
2024-11-20 12:10:16 +0100

    sitemap generator

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -40,12 +40,12 @@ end
 
 # Upload du fichier compressé sur S3
 Aws.config.update(
-  region: ENV['AWS_REGION'],
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+  region: ENV['S3_REGION'],
+  credentials: Aws::Credentials.new(ENV['S3_ACCESS_KEY'], ENV['S3_SECRET_KEY'])
 )
 
 s3 = Aws::S3::Resource.new
-bucket = s3.bucket(ENV['AWS_BUCKET'])
+bucket = s3.bucket(ENV['S3_BUCKET_NAME'])
 
 SitemapGenerator::Sitemap.sitemaps.each do |file|
   # Téléchargement du fichier compressé
