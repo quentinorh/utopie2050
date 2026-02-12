c10fea3ab7164d1174c689aa19c4e447b925673e
Quentin Orhant <quentin.orhant@mailo.fr>
2026-02-12 10:58:21 +0100

    change menu and fields style

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -4,14 +4,51 @@ export default class extends Controller {
  
   static targets = ["avatar", "menuDesktop", "menuSignin", "username", "closeMenu", "signin"]
 
+  connect() {
+    // Close menus when clicking outside
+    this.handleClickOutside = this.handleClickOutside.bind(this)
+    document.addEventListener("click", this.handleClickOutside)
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.handleClickOutside)
+  }
+
   toogleMenu(event) {
-    this.menuDesktopTarget.classList.toggle('hidden')
+    event.stopPropagation()
+    this.menuDesktopTarget.classList.toggle('is-open')
     this.usernameTarget.classList.toggle('hidden')
     this.closeMenuTarget.classList.toggle('hidden')
+
+    // Close sign-in menu if open
+    if (this.hasMenuSigninTarget) {
+      this.menuSigninTarget.classList.remove('is-open')
+    }
   } 
 
   toogleSignin(event) {
-    this.menuSigninTarget.classList.toggle('hidden')
+    event.stopPropagation()
+    this.menuSigninTarget.classList.toggle('is-open')
+
+    // Close desktop menu if open
+    if (this.hasMenuDesktopTarget) {
+      this.menuDesktopTarget.classList.remove('is-open')
+    }
+  }
+
+  handleClickOutside(event) {
+    if (this.hasMenuDesktopTarget && this.menuDesktopTarget.classList.contains('is-open')) {
+      if (!this.menuDesktopTarget.contains(event.target)) {
+        this.menuDesktopTarget.classList.remove('is-open')
+        this.usernameTarget.classList.remove('hidden')
+        this.closeMenuTarget.classList.add('hidden')
+      }
+    }
+
+    if (this.hasMenuSigninTarget && this.menuSigninTarget.classList.contains('is-open')) {
+      if (!this.menuSigninTarget.contains(event.target)) {
+        this.menuSigninTarget.classList.remove('is-open')
+      }
+    }
   }
 }
-
