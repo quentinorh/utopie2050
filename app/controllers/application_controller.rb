0c5b434a371440c8248d306d1b9ae94e28a858a2
Quentin Orhant <quentin.orhant@mailo.fr>
2026-02-12 09:20:33 +0100

    CSRF token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,22 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from ActionController::InvalidAuthenticityToken, with: :handle_invalid_authenticity_token
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :age])
+  end
+
+  private
+
+  def handle_invalid_authenticity_token
+    if request.path == "/users/sign_in" && request.post?
+      redirect_to new_user_session_path, alert: "Votre session a expiré. Veuillez réessayer."
+    else
+      redirect_to root_path, alert: "Votre session a expiré. Veuillez réessayer."
+    end
   end
 end
